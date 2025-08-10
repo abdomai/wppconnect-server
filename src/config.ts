cbb7fba1462ee86bb8ea63cd92a0b675fd11fcb3
abdulmalek <abdulmalek@nabaai.com>
2025-08-10 21:37:33 +0300

    fix the origin to be public

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { ServerOptions } from './types/ServerOptions';
 
 export default {
   secretKey: 'THISISMYSECURETOKEN',
-  host: 'http://localhost',
+  host: 'http://0.0.0.0',
   port: '21460',
   deviceName: 'WppConnect',
   poweredBy: 'WPPConnect-Server',
